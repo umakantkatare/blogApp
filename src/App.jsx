8eac5e2d5e9b44303e853f70e972a08469fef6d9
Umakant Katare <umakantkatare9753121329@gmail.com>
2024-03-09 15:12:57 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-
+/* eslint-disable no-unused-vars */
+import { useState } from 'react'
+import authService from './appwrite/Auth'
 import './App.css'
+import {useDispatch} from 'react-redux'
+import { useEffect } from 'react'
+import {login, logout} from './store/AuthSlice'
+import { Footer, Header } from './components/Index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-    <h1>
+// console.log(import.meta.env.VITE_APPWRITE_URL)
+const [loading, setLoading] = useState(true)
+const dispatch = useDispatch()
 
-    blogApp with appwrite
-    </h1>
-    </>
-  )
+useEffect(() => {
+  authService.getCurrentUser()
+  .then((userData) => {
+   if (userData) {
+    dispatch( login({userData}))
+   }
+   else{
+    dispatch(logout())
+  }
+  })
+  .catch( console.log('error'))
+  .finally(() =>  setLoading(false))
+}, [])
+
+  return !loading ? (
+    <div className=' min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className=' w-full block'>
+        <Header/>
+        <main>
+          <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
